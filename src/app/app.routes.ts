f79478c51ba01eb791832514b89b5fcca4463da8
Nikolay Dimitrov <n.zhdimitrov@gmail.com>
2024-12-09 18:58:31 +0200

    Added Not Found page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RecipesComponent } from "./pages/recipes/recipes.component";
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { CreateRecipeComponent } from "./pages/create-recipe/create-recipe.component";
 import { RecipeDetailsComponent } from "./pages/recipe-details/recipe-details.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
 
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
@@ -19,7 +20,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [guestGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [guestGuard] },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  { path: '**', redirectTo: '' }
+  { path: '**', component: NotFoundComponent },
 ];
 
 export const appRouting = routes;
